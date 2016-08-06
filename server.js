de7929c2a717a99f0c14f8347d7ad8b7725c8ea7
Jeremy <jer.wang25@gmail.com>
2016-08-06 13:47:35 -0500

    added template UI layout

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var port = process.env.PORT || 8080;
+
+server.listen(8080, function(){
+  console.log('Starting server - listening on port :8080');
+});
+
+// Routing
+app.use(express.static(__dirname + '/public'));
+
+// app.get('/', function(req, res){
+//   res.sendfile('public/index.html');
+// })
+// ;
