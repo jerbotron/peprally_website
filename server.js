8f1e3f7f0063f025e65a8a6505b4062358ec4f28
Jeremy <jer.wang25@gmail.com>
2016-09-22 19:46:36 -0500

    fixed email script

--- server.js
+++ server.js
@@ -1,11 +1,44 @@
 var express = require('express');
+var nodemailer = require('nodemailer');
+var smtpTransport = require("nodemailer-smtp-transport")
 var app = express();
 var server = require('http').createServer(app);
 var port = process.env.PORT || 8080;
 
+var transport = nodemailer.createTransport(smtpTransport({
+	host: 'smtp.gmail.com',
+	secureConnection: false, // use SSL
+	port: 587, // port for secure SMTP
+	auth: {
+    	user: 'peprallyapp@gmail.com',
+    	pass: 'peprallytestuser'
+ 	}	
+}));
+
+// Routing
+// Serve static pages
+app.use(express.static(__dirname + '/public'));
+// Contact me email script
+app.get('/send',function(req,res){
+    var message = "Phone number: " + req.query.phone + "\nEmail: " + req.query.email + "\n\n" + req.query.message;
+    var mailOptions={
+    	from: '"' + req.query.name + '" <' + req.query.email + '>',
+        to: 'wyjeremy@gmail.com',
+        subject: "peprallyapp.co Inquiry",
+        text: message
+    }
+    console.log(mailOptions);
+    transport.sendMail(mailOptions, function(error, response){
+        if (error){
+        	console.log(error);
+        	res.end("error");
+        } else {
+        	console.log("Message sent");
+        	res.end("sent");
+        }
+    });
+});
+
 server.listen(8080, function(){
   console.log('Starting server - listening on port :8080');
 });
-
-// Routing
-app.use(express.static(__dirname + '/public'));
